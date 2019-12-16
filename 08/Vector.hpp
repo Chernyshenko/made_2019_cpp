18f9c5f29cd5b16e226395524bfaf04604d19826
Chernyshenko <chernyshenko.a@gmail.com>
2019-12-17 00:38:51 +0300

    correction of random access vector, correction in reallocate

--- 08/Vector.hpp
+++ 08/Vector.hpp
@@ -42,11 +42,60 @@ public:
         return *this;
     }
 
+    Iterator& operator+=(int n)
+    {
+        ptr_ = ptr_ + n;
+        return *this;
+    }
+    Iterator& operator+(int n)
+    {
+        T* res = ptr_ + n;
+        return res;
+    }
+    Iterator& operator+(const Iterator<T>& other)
+    {
+        Iterator res(ptr_ + other.ptr_);
+        return res;
+    }
+
     Iterator& operator--()
     {
         --ptr_;
         return *this;
     }
+    Iterator& operator-=(int n)
+    {
+        ptr_ = ptr_ - n;
+        return *this;
+    }
+    Iterator& operator-(int n)
+    {
+        Iterator res(ptr_ - n);
+        return res;
+    }
+
+    Iterator& operator-(const Iterator<T>& other)
+    {
+        Iterator res(ptr_ - other.ptr_);
+        return res;
+    }
+    bool operator<(const Iterator<T>& other) const{
+        return ptr_ < other.ptr_;
+    }
+    bool operator>(const Iterator<T>& other) const{
+        return other.ptr_ < ptr_;
+    }
+    bool operator>=(const Iterator<T>& other) const{
+        return !(ptr_ < other.ptr_);
+    }
+    bool operator<=(const Iterator<T>& other) const{
+        return !(other.ptr_ < ptr_);
+    }
+    reference operator[](int n) const{
+        return *(ptr_ + n);
+    }
+
+
 };
 
 template <class T, class Alloc = Allocator<T> >
@@ -105,7 +154,11 @@ private:
 template <class T, class Alloc>
 void Vector<T, Alloc>:: reallocate(size_type newsize){
     T* mem = alloc_.allocate(newsize);
-    std::move(data, data + size_, mem);
+    T* dst = mem;
+    for(T* src = data; src != data + size_; ++src, ++dst){
+        alloc_.construct(dst, std::move(*src));
+        alloc_.destroy(src);          
+    }
     alloc_.deallocate(data, capacity_);
     data = mem;
     mem = nullptr;
@@ -123,7 +176,9 @@ Vector<T, Alloc>::Vector(size_type count) : capacity_(count), size_(count) {
 template <class T, class Alloc>
 Vector<T, Alloc>::Vector(size_type count, const value_type& defaultValue) : capacity_(count), size_(count){
     data = alloc_.allocate(capacity_);
-    std::fill(data, data + size_, defaultValue );
+    for(size_type i = 0; i < size_; i++){
+        alloc_.construct(data + i, defaultValue);
+    }
 }
 template <class T, class Alloc>
 Vector<T, Alloc>::~Vector(){
@@ -193,6 +248,7 @@ void Vector<T, Alloc>::push_back(const value_type& value){
 template <class T, class Alloc>
 void Vector<T, Alloc>::pop_back(){
     assert (size_ > 0);
+    alloc_.destroy(data + size_ - 1);
     size_--;
 }
 template <class T, class Alloc>
